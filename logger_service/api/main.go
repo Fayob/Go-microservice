5a31817cb71ab1f6f24297651867020e6c844b5b
fayob <adedejifavour6@gmail.com>
2023-08-25 11:27:13 +0100

    Add rpc functionality to logger service

--- logger_service/api/main.go
+++ logger_service/api/main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"github.com/fayob/go_micro/logger_service/data"
@@ -48,6 +50,13 @@ func main()  {
 		Models: data.New(client),
 	}
 
+	// Register the RPC Server
+	err = rpc.Register(new(RPCServer))
+	if err != nil {
+		log.Println(err)
+	}
+	go app.rpcListen()
+
 	// serve web server
 	srv := &http.Server{
 		Addr: fmt.Sprintf(":%s", webPort),
@@ -57,6 +66,23 @@ func main()  {
 	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port ", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
 	}
 }
 
